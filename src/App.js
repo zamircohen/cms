ea70c7f1149bcbe0b410963c7fa3cdb012da054c
zamircohen <zamir_cohen@hotmail.com>
2022-09-13 18:50:47 +0200

    Add a Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import ItemPage from "./pages/ItemPage";
 import ListPage from "./pages/ListPage";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ListPage />} />
         <Route path="/:id" element={<ItemPage />} />
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className="nav">
+            <ul>
+                <li><a href="/" className="nav-link">Home</a></li>
+            </ul>
+        </div>
+    )
+}
